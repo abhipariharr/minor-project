2ac2ee7a738600a45f86050a0413dec165f29a53
Abhishek parihar <114382172+abhipariharr@users.noreply.github.com>
2023-03-02 20:01:42 +0530

    Update collegeform.c

--- collegeform.c
+++ collegeform.c
@@ -6,23 +6,30 @@ int main()
 {
     char a,b,d,e;
     int age;
+    long int f,g;
 
-    printf("full name\n");
-    scanf("%s", &a);
     printf("age\n");
     scanf("%d", &age);
     if(age>=18)
     {
-        printf("your address\n");
+        printf("Enter first name\n");
+        scanf("%s", &a);
+        printf("Your address\n");
         scanf("%s", &b);
-        printf("Father's name\n");
+        printf("Father's first name\n");
         scanf("%s", &d);
-        printf("mothers name\n");
+        printf("Mothers name\n");
         scanf("%s", &e);
+        printf("Enter your phone number\n");
+        scanf("%ld",&f);
+        printf("Enter your class 12th marks\n=");
+        scanf("%ld",&g);
+
+        printf("\n\nThank you for submitting your information\nWe will notify you your exam date !!");
     }
     else
     {
-        printf("wrong choice");
+        printf("Your are not eligible to give exam");
     }
     getch();
 }
